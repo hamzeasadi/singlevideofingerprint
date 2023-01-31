696330f8f51962ce16066ab1be1302698cb4e033
hamzeasadi <hamzeasadi1@gmail.com>
2023-01-31 15:41:39 +0000

    status new model

--- preprocessing.py
+++ preprocessing.py
@@ -3,13 +3,18 @@ import conf as cfg
 
 
 
-def extract_iframes(videospath, iframespath):
-    videofiles = os.listdir(videospath)
-    videofiles = cfg.rm_ds(videofiles)
-    for i, videofile in enumerate(videofiles):
-        videopath = os.path.join(videospath, videofile)
-        command = f"ffmpeg -skip_frame nokey -i {videopath} -vsync vfr -frame_pts true -x264opts no-deblock {iframespath}/video{i}iframe%d.bmp"
-        os.system(command=command)
+def extract_iframes(videospath, iframespath): 
+    videofolders = os.listdir(videospath) 
+    videofolders = cfg.rm_ds(videofolders) 
+    for f, videofolder in enumerate(videofolders): 
+        videofolderpath = os.path.join(videospath, videofolder) 
+        trgiframesfolderpath = os.path.join(iframespath, videofolder) 
+        cfg.createdir(trgiframesfolderpath) 
+        videofiles = os.listdir(videofolderpath) 
+        for v, videofile in enumerate(videofiles): 
+            videofilepath = os.path.join(videofolderpath, videofile) 
+            command = f"ffmpeg -skip_frame nokey -i {videofilepath} -vsync vfr -frame_pts true -x264opts no-deblock {trgiframesfolderpath}/video{v}iframe%d.bmp" 
+            os.system(command=command)
 
 
 
